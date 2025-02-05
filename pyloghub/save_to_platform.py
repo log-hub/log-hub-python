34c0e5a2ea8a7e6cf4ba0cbd786466f709a52dae
bojana <bojana.vuckovic@log-hub.com>
2025-02-05 16:23:15 +0100

    savescenario adjusted for supply chain maps

--- pyloghub/save_to_platform.py
+++ pyloghub/save_to_platform.py
@@ -17,28 +17,33 @@ def get_app_name(url):
     return app_name
 
 
-def save_scenario_check(save_scenario_data: dict, payload: dict) -> dict:
+def save_scenario_check(save_scenario_data: dict, payload: dict, application_name = "") -> dict:
     """
     Updates the payload with the information about saving a scenario. If any of the parameters for saving a scenario was not passed, it will be filled with the default value.
 
     Parameters:
     save_scenario_data (dict): A dictionary with the parameters for saving a scenario:
-        - saveScenario (True/False), False by default
-        - overwriteScenario (True/False), False by default
-        - workspaceId (id of an existing workspace)
-        - scenarioName 
+        - saveScenario (boolean): False by default
+        - overwriteScenario (boolean): False by default
+        - mergeWithExistingScenario (boolean): False by default
+        - workspaceId (str): Id of an existing workspace
+        - scenarioName (str): Scenario name
     payload (dict): A dictionaty with the data that should be calculated
+    application_name (str): Application name
 
     Returns:
     dict: A dictionary containing input data and the information about saving a scenario.
     """
-    save_scenario_parameters = ['saveScenario','overwriteScenario', 'workspaceId', 'scenarioName']
+    save_scenario_parameters = ['saveScenario', 'overwriteScenario','workspaceId', 'scenarioName']
     save_scenario_default_values = {
         'saveScenario' : False,
         'overwriteScenario': False,
         'workspaceId': "",
         'scenarioName': ""
     }
+    if 'supplychainmap' in application_name:
+        save_scenario_parameters.insert(2, 'mergeWithExistingScenario')
+        save_scenario_default_values.update({'mergeWithExistingScenario': False})
     payload['saveScenarioParameters']  = {}
     for par in save_scenario_parameters:
         payload['saveScenarioParameters'].update({par: save_scenario_data.get(par, save_scenario_default_values[par])})
--- pyloghub/supply_chain_map_locations.py
+++ pyloghub/supply_chain_map_locations.py
@@ -5,43 +5,37 @@ import time
 import logging
 import warnings
 from typing import Optional, Dict, Tuple
-from save_to_platform import save_scenario_check
+from save_to_platform import save_scenario_check, get_app_name
 
-def forward_supply_chain_map_locations(addresses: pd.DataFrame, api_key: str, save_scenario = {}) -> Optional[Tuple[pd.DataFrame, pd.DataFrame]]:
+def forward_supply_chain_map_locations(addresses: pd.DataFrame, api_key: str, save_scenario = {}) -> Optional[pd.DataFrame]:
     """
-    Calculate center of gravity plus based on a list of addresses, their weights, volumes, and revenues.
+    Creates a map of locations based on the given addresses.
 
-    This function takes a DataFrame of addresses with weights, volumes, and revenues, and a set of parameters,
-    along with an API key, and performs center of gravity plus calculation using the Log-hub service.
+    This function takes a DataFrame of addresses with their layer, quantity and descriptions, along with an API key, 
+    and creates a map of locations using the Log-hub Supply Chain Map service. 
 
     Parameters:
-    addresses (pd.DataFrame): A pandas DataFrame containing addresses with their weights, volumes, and revenues.
+    addresses (pd.DataFrame): A pandas DataFrame containing addresses with their layer, quantity and descriptions.
         Each row should contain:
         - id (number): Identifier.
-        - name (str): Name.
+        - name (str): Location name.
         - country (str): Country code. Must have at least 1 character.
         - state (str): State code.
         - postalCode (str): Postal code.
         - city (str): City name.
         - street (str): Street name with house number.
-        - weight (float): Weight.
-        - volume (float): Volume.
-        - revenue (float): Revenue.
-
-    parameters (Dict): A dictionary containing parameters like numberOfCenters (number), distanceUnit (enum: "km" or "mi"),
-                        and importance factors (importanceWeight, importanceVolume, importanceRevenue).
+        - layer (str): Visual settings of locations in the same layer can be adjusted together.
+        - quantity (number): Numerical value affecting the size of the location symbol on the map.
+        - nameDescription1, nameDescription2 (str): Additional location descriptions.
 
     api_key (str): The Log-hub API key for accessing the center of gravity plus service.
 
     save_scenario (dict): A dictionary containg information about saving scenario, empty by default. Allowed key vales are
-                        'saveScenario' (boolean), 'overwriteScenario' (boolean), 'workspaceId' (str) and
-                        'scenarioName' (str).
+                        'saveScenario' (boolean), 'overwriteScenario' (boolean), 'mergeWithExistingScenario (boolean), 'workspaceId' (str) and 'scenarioName' (str).
 
     Returns:
-    Tuple[pd.DataFrame, pd.DataFrame]: A tuple of two pandas DataFrames. The first DataFrame contains the 
-                                       assigned addresses with their respective centers, and the second 
-                                       DataFrame contains the details of the centers.
-                                       Returns None if the process fails.
+    pd.DataFrame: A pandas DataFrame containg the addresses and their parsed latitude and longitude. Returns None if 
+                  the process fails.
     """
 
     def validate_and_convert_data_types(df):
@@ -70,7 +64,7 @@ def forward_supply_chain_map_locations(addresses: pd.DataFrame, api_key: str, sa
     if addresses is None:
         return None
 
-    DEFAULT_LOG_HUB_API_SERVER = "https://production.supply-chain-apps.log-hub.com/"
+    DEFAULT_LOG_HUB_API_SERVER = "https://production.supply-chain-apps.log-hub.com"
     LOG_HUB_API_SERVER = os.getenv('LOG_HUB_API_SERVER', DEFAULT_LOG_HUB_API_SERVER)
     url = f"{LOG_HUB_API_SERVER}/api/applications/v1/supplychainmaplocations"
     
@@ -81,9 +75,9 @@ def forward_supply_chain_map_locations(addresses: pd.DataFrame, api_key: str, sa
     }
     payload = {
         "geocodingData": addresses.to_dict(orient='records'),
-        'saveScenarioParameters': save_scenario
     }
-    #payload = save_scenario_check(save_scenario, payload)
+    app_name = get_app_name(url)
+    payload = save_scenario_check(save_scenario, payload, app_name)
     max_retries = 3
     retry_delay = 15  # seconds
 
@@ -115,7 +109,7 @@ def forward_supply_chain_map_locations_sample_data():
     addresses_df = pd.read_excel(data_path, sheet_name='addresses', usecols='A:K', dtype={'postalCode': str})
 
     save_scenario = {
-        'saveScenario': False,
+        'saveScenario': True,
         'overwriteScenario': False,
         'workspaceId': 'Your workspace id',
         "mergeWithExistingScenario": False,
@@ -123,3 +117,107 @@ def forward_supply_chain_map_locations_sample_data():
     }
     return {'addresses': addresses_df, 'saveScenarioParameters': save_scenario}
 
+def reverse_supply_chain_map_locations(coordinates: pd.DataFrame, api_key: str, save_scenario = {}) -> Optional[pd.DataFrame]:
+    """
+    Creates a map of locations based on the given coordinates.
+
+    This function takes a DataFrame of coordinates with their layer, quantity and descriptions, along with an API key, 
+    and creates a map of locations using the Log-hub Supply Chain Map service. 
+
+    Parameters:
+    coordinates (pd.DataFrame): A pandas DataFrame containing coordinates with their layer, quantity and descriptions.
+        Each row should contain:
+        - id (number): Identifier.
+        - name (str): Location name.
+        - latitude (number): Latitude of the location.
+        - longitude (number): Longitude of the location.
+        - layer (str): Visual settings of locations in the same layer can be adjusted together.
+        - quantity (number): Numerical value affecting the size of the location symbol on the map.
+        - nameDescription1, nameDescription2 (str): Additional location descriptions.
+
+    api_key (str): The Log-hub API key for accessing the center of gravity plus service.
+
+    save_scenario (dict): A dictionary containg information about saving scenario, empty by default. Allowed key vales are
+                        'saveScenario' (boolean), 'overwriteScenario' (boolean), 'mergeWithExistingScenario (boolean), 'workspaceId' (str) and 'scenarioName' (str).
+
+    Returns:
+    pd.DataFrame: A pandas DataFrame containg the coordinates. Returns None if the process fails.
+    """
+
+    def validate_and_convert_data_types(df):
+        """
+        Validate and convert the data types of the DataFrame columns.
+        Log an error message if a required column is missing or if conversion fails.
+        """
+        required_columns = {
+            'id': 'float', 'name': 'str', 'latitude':'float', 'longitude': 'float', 'layer': 'str', 'quantity': 'float', 'nameDescription1': 'str', 'nameDescription2': 'str'
+        }
+        for col, dtype in required_columns.items():
+            if col in df.columns:
+                try:
+                    df[col] = df[col].astype(dtype)
+                except Exception as e:
+                    logging.error(f"Data type conversion failed for column '{col}': {e}")
+                    return None
+            else:
+                logging.error(f"Missing required column: {col}")
+                return None
+        return df
+
+    # Validate and convert data types
+    coordinates = validate_and_convert_data_types(coordinates)
+    if coordinates is None:
+        return None
+
+    DEFAULT_LOG_HUB_API_SERVER = "https://production.supply-chain-apps.log-hub.com"
+    LOG_HUB_API_SERVER = os.getenv('LOG_HUB_API_SERVER', DEFAULT_LOG_HUB_API_SERVER)
+    url = f"{LOG_HUB_API_SERVER}/api/applications/v1/reversesupplychainmaplocations"
+    
+    headers = {
+        "accept": "application/json",
+        "authorization": f"apikey {api_key}",
+        "content-type": "application/json"
+    }
+    payload = {
+        "geocodingData": coordinates.to_dict(orient='records'),
+    }
+    app_name = get_app_name(url)
+    payload = save_scenario_check(save_scenario, payload, app_name)
+    max_retries = 3
+    retry_delay = 15  # seconds
+
+    for attempt in range(max_retries):
+        try:
+            response = requests.post(url, json=payload, headers=headers)
+            if response.status_code == 200:
+                response_data = response.json()
+                geocoding_result_df = pd.DataFrame(response_data['geocodingData'])
+                return geocoding_result_df
+            elif response.status_code == 429:
+                logging.info(f"Rate limit exceeded. Retrying in {retry_delay} seconds.")
+                time.sleep(retry_delay)
+            else:
+                logging.error(f"Error in center of gravity plus API: {response.status_code} - {response.text}")
+                return None
+        except requests.exceptions.RequestException as e:
+            logging.error(f"Request failed: {e}")
+            if attempt < max_retries - 1:
+                logging.info(f"Retrying in {retry_delay} seconds.")
+                time.sleep(retry_delay)
+
+    logging.error("Max retries exceeded.")
+    return None
+
+def reverse_supply_chain_map_locations_sample_data():
+    warnings.simplefilter("ignore", category=UserWarning)
+    data_path = os.path.join(os.path.dirname(__file__), 'sample_data', 'MapLocationsReverse.xlsx')
+    addresses_df = pd.read_excel(data_path, sheet_name='coordinates', usecols='A:H', dtype={'postalCode': str})
+
+    save_scenario = {
+        'saveScenario': True,
+        'overwriteScenario': False,
+        'workspaceId': 'Your workspace id',
+        "mergeWithExistingScenario": False,
+        'scenarioName': 'Your scenario name'
+    }
+    return {'coordinates': addresses_df, 'saveScenarioParameters': save_scenario}
